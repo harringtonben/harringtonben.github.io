4a8332b48f4078a76b989d346c13578af1182f02
ben harrington <harringtonben@icloud.com>
2017-08-30 21:30:21 -0500

    finished bootstrap for blog

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var searchMe = document.getElementById("searchText");
+
 
 var getAllTheBlogs = new XMLHttpRequest;
 getAllTheBlogs.addEventListener("load", bringMeTheBlogs);
@@ -22,7 +24,6 @@ function printBlog(blogs) {
 		var domString = "";
 		domString += `<div class="col-sm-6 col-md-4">
 					    <div class="thumbnail">
-					      <img src="..." alt="...">
 					      <div class="caption">
 					        <h3>${blogs[i].heading}</h3>
 					        <h4>${blogs[i].date}</h4>
@@ -34,4 +35,40 @@ function printBlog(blogs) {
 	    blogContainer.innerHTML += domString;
 	}
 }
+
+document.body.addEventListener("click", function(event) {
+	// console.log(event);
+	if (event.target.parentNode.parentNode.parentNode.className === "col-sm-6 col-md-4") {
+		var printMe = event.target.parentNode.parentNode.parentNode.innerHTML;
+		console.log(printMe);
+		printDatCard(printMe);
+	} else if (event.target.className === "caption"){
+		printMe = event.target.parentNode.parentNode.innerHTML;
+		console.log(event);
+		printDatCard(printMe);
+
+	}
+})
+
+function printDatCard(printing) {
+	var giveMetext = document.getElementById("fillme");
+	giveMetext.innerHTML = printing;
+
+}
+
+// searchMe.addEventListener('keypress', function(event) {
+//   // console.log("event", event.keyCode);
+//   if (event.key === 'Enter') {
+//     var txt = searchMe.value;
+//     //1. filter planets array
+//     var results = blogContainer.filter(function(thing){
+//       // console.log('filter thing', thing);
+//       return thing.name.indexOf(txt)>-1;
+//     })
+//     printBlog(results);
+//     // domString(results);
+//     // console.log(results);
+//     // console.log('you hit enter bitch', txt);
+//   }
+// })
 
