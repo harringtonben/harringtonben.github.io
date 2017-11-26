04943d920e77484cd59e0bd45793a3ed952aed2f
ben harrington <harringtonben@icloud.com>
2017-11-26 08:50:21 -0600

    finsihed personal site minus a CSS bug I need to work out

--- javascripts/controllers/ProjectCtrl.js
+++ javascripts/controllers/ProjectCtrl.js
@@ -1,5 +1,14 @@
 'use strict';
 
-app.controller("ProjectCtrl", function($scope) {
-    $scope.controller = "ProjectCtrl";
+app.controller("ProjectCtrl", function($scope, DatabaseService) {
+
+    const getProjects = () => {
+        DatabaseService.getProjects().then((results) => {
+            $scope.projects = results;
+        }).catch((error) => {
+            console.log("Error in getProjects", error);
+        });
+    };
+   
+    getProjects();
 });
--- javascripts/services/DatabaseService.js
+++ javascripts/services/DatabaseService.js
@@ -19,6 +19,24 @@ app.service("DatabaseService", function($http, $q, FIREBASE_CONFIG) {
             });
         });
     };
+
+    const getProjects = () => {
+        let myProjects = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/projects.json`).then((results) => {
+                let projects = results.data;
+                if (projects != null) {
+                    Object.keys(projects).forEach((key) => {
+                        projects[key].id = key;
+                        myProjects.push(projects[key]);
+                    });    
+                }
+                resolve(myProjects);
+            }).catch((error) => {
+                console.log("error in database getProjects", error);
+            });
+        });
+    };
     
-    return {getBlogs};
+    return {getBlogs, getProjects};
 });
